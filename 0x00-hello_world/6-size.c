b6a6a2ddf5f0ac3d0de7ca2cfb82fd5da7bd84eb
amish444 <aminahahmedds@gmail.com>
2022-09-08 13:14:12 -0700

    hjg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -15,8 +14,8 @@ int main(void)
 	float f;
 	printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of a long int: %1u byte(s)\n" , (unsigned long)sizeof(b));
-	printf("size of a long long int: %1u byte (s)\n" , (unsigned long)sizeof(d));
-	printf("size of a float: %1u byte(s)\n" , (unsigned long)sizeof(f));
+	printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %1u byte (s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
