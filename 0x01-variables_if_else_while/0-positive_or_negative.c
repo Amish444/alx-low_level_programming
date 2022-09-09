ec9178b5662198ebd817a85d831ff2d5806b3074
amish444 <aminahahmedds@gmail.com>
2022-09-09 06:17:01 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,50 +1,34 @@
-nclude <stdio.h>                                                                                                                                       
-
-#include <time.h>                                                                                                                                        
-
+#include <stdio.h>                                                                                                                                       
+#include <time.h>                                                                                                                                       
 #include <stdio.h>                                                                                                                                       
 
 /**                                                                                                                                                      
  *
  *  * main -Entry point                                                                                                                                     
+ * Return:0                      
  *
- *   *                                                                                                                                                       
- *
- *    * Return:0                                                                                                                                              
- *
- *     */                                                                                                                                                      
+  */                                                                                                                                                      
 
 int main(void)                                                                                                                                           
 
 {                                                                                                                                                        
 
-	        int n;                                                                                                                                           
-
-		        srand(time(0));                                                                                                                                  
+	        int n;                                                                                                                                  
+		 srand(time(0));
+n=		 
 
 			        if (n==0)                                                                                                                                        
 
-					        {                                                                                                                                                
-
-							                printf("i is zero\n");                                                                                                                   
-
-									        }                                                                                                                                                
-
-				else if (n<0)                                                                                                                                            
-
+					        {                                                                                                                                               
+							                printf("i is zero\n");                                                                                         
+									        }                                                                                   
+				else if (n<0)                                                                                                            
 				{                                                                                                                                                        
-
-					        printf("%i is negative\n",n);                                                                                                                    
-
-				}                                                                                                                                                        
-
-				else                                                                                                                                                     
-
-				{                                                                                                                                                        
-
-					        printf("%i is postive\n",);                                                                                                                      
-
-				}                                                                                                                                                        
+					        printf("%i is negative\n")
+				}                                                                                                                        
+				else                                                                                                                     
+				{                                                                                                                        
+					        printf("%i is postive\n",);                                                                                                                      	}                                                                                                                                                        
 
 				return (0);                                                                                                                                              
 
