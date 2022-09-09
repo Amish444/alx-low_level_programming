ff53fa879674d9c4acd5e1d2dfc1e894f6aacbfe
amish444 <aminahahmedds@gmail.com>
2022-09-09 06:02:23 -0700

    l

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,51 @@
+nclude <stdio.h>                                                                                                                                       
+
+#include <time.h>                                                                                                                                        
+
+#include <stdio.h>                                                                                                                                       
+
+/**                                                                                                                                                      
+ *
+ *  * main -Entry point                                                                                                                                     
+ *
+ *   *                                                                                                                                                       
+ *
+ *    * Return:0                                                                                                                                              
+ *
+ *     */                                                                                                                                                      
+
+int main(void)                                                                                                                                           
+
+{                                                                                                                                                        
+
+	        int n;                                                                                                                                           
+
+		        srand(time(0));                                                                                                                                  
+
+			        if (n==0)                                                                                                                                        
+
+					        {                                                                                                                                                
+
+							                printf("i is zero\n");                                                                                                                   
+
+									        }                                                                                                                                                
+
+				else if (n<0)                                                                                                                                            
+
+				{                                                                                                                                                        
+
+					        printf("%i is negative\n",n);                                                                                                                    
+
+				}                                                                                                                                                        
+
+				else                                                                                                                                                     
+
+				{                                                                                                                                                        
+
+					        printf("%i is postive\n",);                                                                                                                      
+
+				}                                                                                                                                                        
+
+				return (0);                                                                                                                                              
+
+				
