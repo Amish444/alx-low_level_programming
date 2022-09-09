f854e56d16792b6866f301947dde91d5f2fae98e
amish444 <aminahahmedds@gmail.com>
2022-09-09 06:49:52 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,31 @@
-#include <stdio.h>                                                                                                                                       
-#include <time.h>                                                                                                                                       
-#include <stdio.h>                                                                                                                                       
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-/**                                                                                                                                                      
+/**
+ * main -Entry point
  *
- *  * main -Entry point                                                                                                                                     
- * Return:0                      
- *
-  */                                                                                                                                                      
+ * Return:0
+ */
 
-int main(void)                                                                                                                                           
+int main(void)
+{
+	int n;
 
-{                                                                                                                                                        
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	        int n;                                                                                                                                  
-		 srand(time(0));
-n=		 
-
-			        if (n==0)                                                                                                                                        
-
-					        {                                                                                                                                               
-							                printf("i is zero\n");                                                                                         
-									        }                                                                                   
-				else if (n<0)                                                                                                            
-				{                                                                                                                                                        
-					        printf("%i is negative\n")
-				}                                                                                                                        
-				else                                                                                                                     
-				{                                                                                                                        
-					        printf("%i is postive\n",);                                                                                                                      	}                                                                                                                                                        
-
-				return (0);                                                                                                                                              
-
-				
+	if (n == 0)
+	{
+		printf("%i is zero\n",n);
+	}
+	else if (n<0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else 
+	{
+		printf("%i is positive\n", n);
+	}
+	return (0);
+}
